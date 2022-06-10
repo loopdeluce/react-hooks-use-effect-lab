2899015d3880fa6b8a9524e9e6fa9de21385cb34
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-10 11:37:40 -0600

    commit tree 520a2bc8e0a5401929a75dc3fed64b33c254f9d6

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => setTimeRemaining((timeRemaining) => timeRemaining-1), 1000);
+    return () => clearTimeout(timer);
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
+  }
+
+  if (timeRemaining === 0) {
+    setTimeRemaining(10);
+    onAnswered(false);
   }
 
   const { id, prompt, answers, correctIndex } = question;
